4a6bbe2f7e0d5751d341aec7d6963c4bd55e07c3
Bigloso <wayside.big@gmail.com>
2020-03-20 16:14:19 +0700

    v1

--- projectComproV1.cpp
+++ projectComproV1.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+int main(){
+    string name ;
+    string word ;
+    cout << "Hotel: Hello can you tell me your name ?\n";
+    cout << "??????:" ;
+    getline(cin,name) ;
+    cout << "Hotel: Do you want to hotel reservation ?\n" ;
+    cout << name << ": " ;
+    getline(cin,word) ;
+
+    return 0 ;
+}
