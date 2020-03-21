847d4c81d62c3fd712d3970deb2c4bdb35c690dd
Bigloso <wayside.big@gmail.com>
2020-03-21 10:32:00 +0700

    ver2

--- projectComproV1.cpp
+++ projectComproV1.cpp
@@ -7,12 +7,32 @@ using namespace std ;
 int main(){
     string name ;
     string word ;
+    int start = 0 ;
     cout << "Hotel: Hello can you tell me your name ?\n";
     cout << "??????:" ;
     getline(cin,name) ;
-    cout << "Hotel: Do you want to hotel reservation ?\n" ;
+    cout << "Hotel: Is there anything We can help you with ?\n" ;
     cout << name << ": " ;
     getline(cin,word) ;
-
+    int end = word.find_first_of(" ") ;
+    while(end != -1){
+    if(word.substr(start,end-start) == "reservation"){
+        cout << "bigloso" ;
+        end = word.find_first_of(" ") ;
+        break ;
+        }
+        start = end+1 ;
+        end = word.find_first_of(" ",start);
+    }
+    while(end != -1){
+    if(word.substr(start,end-start) == "description"){
+        cout << "bigloso" ;
+        end = word.find_first_of(" ") ;
+        break ;
+        }
+        start = end+1 ;
+        end = word.find_first_of(" ",start);
+    }
+    
     return 0 ;
 }
