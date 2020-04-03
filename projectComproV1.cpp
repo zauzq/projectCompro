bd440e180afc7dae0442e600130a530b3654c6ce
bigloso <59277363+bigloso@users.noreply.github.com>
2020-04-03 16:46:23 +0700

    verr

--- projectComproV1.cpp
+++ projectComproV1.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <conio.h>
@@ -10,78 +9,82 @@ using namespace std;
 using namespace std ;
 
 void Shownamehotel(){
-    // system("mode 100,40");
-    // system("COLOR AF");
-    cout << "        **************************************************************\n" ;
-    cout << "        *                                                            *\n" ;
-    cout << "        *                   Compro project Hotel                     *\n";
-    cout << "        *                                                            *\n" ;
-    cout << "        **************************************************************\n" ;
-    // getch();
+
+    cout << "                           **************************************************************\n" ;
+    cout << "                           * *  *                                                  *  * *\n" ;
+    cout << "                           **  *                                                    *  **\n" ;
+    cout << "                           * *                 Compro project Hotel                   * *\n";
+    cout << "                           **  *                                                    *  **\n" ;
+    cout << "                           * *  *                                                  *  * *\n" ;
+    cout << "                           **************************************************************\n" ;
+    
 }
 
 int main(){
     int age , hmn , room , hmr ,room1 ,room2, sum1,sum2 ,total ,yn,g ;
     string name ,surname, wf ,number ;
+    string space = "                           " ;
     system("mode 0,0");
     system("COLOR 3F");
     do
     {
         system("cls");
         Shownamehotel();
-            cout<< endl << "        Please input your presonal information"<<endl;
-            cout<< endl << "        **************************************************************"<<endl;
+            cout<< endl << space << "Please input your presonal information"<<endl;
+            cout<< endl << space <<  "______________________________________________________________"<<endl;
             cout<<endl;
-            cout<<"        Please input your name : ";
+            cout<< space << "Please input your name : ";
             cin>>name ;
-            cout<<"        Please input your surname : ";
+            cout<< space << "Please input your surname : ";
             cin>>surname ;
-            cout<<"        Please input your age : ";
+            cout<< space << "Please input your age : ";
             cin>> age ;
-            cout<<"        Please input your phone number : " ;
+            cout<< space << "Please input your phone number : " ;
             cin >> number ;
             system("cls") ;
 
         Shownamehotel() ;
             cout << endl ;
-            cout<<"        Please input your presonal information"<<endl;
+            cout<< space << "Please input your presonal information"<<endl;
             cout<<endl;
-            cout<<"        **************************************************************"<<endl;
+            cout<< space << "______________________________________________________________"<<endl;
             cout<<endl;
-            cout<<"        Name : "<<name <<" "<<surname<<endl<<endl ;
-            cout<<"        ______________________________________________________________"<<endl<<endl ;
-            cout<<"        When for ?"<<endl ;
-            cout<<"        Day (d/m/25xx) :" ;
+            cout<< space << "Name : "<<name <<" "<<surname<<endl ;
+            cout<< space << "Age  : "<<age <<endl ;
+            cout<< space << "Phone number :"<< number <<endl;
+            cout<< space << "______________________________________________________________"<<endl<<endl ;
+            cout<< space << "When for ?"<<endl ;
+            cout<< space << "Day (d/m/25xx) :" ;
             cin>>wf ;
-            cout<<"        How many night ? :" ;
+            cout<< space << "How many night ? :" ;
             cin>>hmn ;
-            cout<<"        How many guests will thre be ? :" ;
-            cin >> g ;
-            cout << endl << "        And would you like a romm ?"<<endl ;
-            cout << "         1. A double room. 800 bath/night :";
-            cin >> room1 ;
+            cout<< space << "How many guests will thre be ? :" ;
+            cin >> g  ;
+            cout<<space << "And would you like a romm ?"<<endl ;
+            cout << space <<  "1. A double room. 800 bath/night :";
+            cin >> room1  ;
             sum1=(800*room1)*hmn;
-            cout << "         2. A twin room. 600 bath/night :" ;
+            cout << space <<  "2. A twin room. 600 bath/night :" ;
             cin >> room2 ;
             sum2=(600*room2)*hmn;
             total = sum1+sum2 ;
-            cout<<"        Price : "<<total<<" bath"<<endl ;
+            cout<< space << "Price : "<<total<<" bath"<<endl ;
         system("cls") ;
         Shownamehotel() ;
-            cout<<"        Name : "<<name<<" "<<surname<<endl;
-            cout<<"        Age : "<<" years old"<<endl ;
-            cout<<"        Tel. : "<<number<<endl ;
-            cout<<"        Day "<<wf<<endl ;
-            cout<<"        How long will you stay : "<<hmn<<" night"<<endl ;
-            cout<<"        guests : "<<g<<" people"<<endl;
-            cout<<"         1. A double room. : "<<room1<<endl ;
-            cout<<"         2. A twin room. : "<<room2<<endl ;
-            cout<<"        Price : "<<total<<" bath"<<endl ;
-            cout<<"        ______________________________________________________________" ;
+            cout<< space << "Name : "<<name<<" "<<surname<<endl;
+            cout<< space << "Age : "<<" years old"<<endl ;
+            cout<< space << "Tel. : "<<number<<endl ;
+            cout<< space << "Day "<<wf<<endl ;
+            cout<< space << "How long will you stay : "<<hmn<<" night"<<endl ;
+            cout<< space << "guests : "<<g<<" people"<<endl;
+            cout<< space << " 1. A double room. : "<<room1<<endl ;
+            cout<< space << " 2. A twin room. : "<<room2<<endl ;
+            cout<< space << "Price : "<<total<<" bath"<<endl ;
+            cout<< space << "______________________________________________________________" ;
             cout<<endl ;
-            cout<<"        Please verty that the information is correct or not."<<endl ;
-            cout<<"        Yes =1 or No =0"<<endl;
-            cout<<"        Input number : ";
+            cout<< space << "Please verty that the information is correct or not."<<endl ;
+            cout<< space << "Yes =1 or No =0"<<endl;
+            cout<< space << "Input number : ";
             cin >> yn ;
     }
     while(yn == 0) ;
@@ -90,7 +93,7 @@ int main(){
             system("cls");
             Shownamehotel();
             cout << endl ;
-            cout << "        ********************thaks completion list*********************" ;
+            cout << space <<  "********************thaks completion list*********************" ;
         }
         getch();
     return 0 ;
